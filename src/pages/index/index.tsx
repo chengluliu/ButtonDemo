b3cec4e73edb1f896f60326681256e3e0e8daff6
风影 <15766799@qq.com>
2024-05-23 11:34:08 +0800

    ActionSheet 动作面板  样式bug

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,30 @@
-import { Button, Radio } from '@nutui/nutui-react-taro';
+import { useState } from 'react'
+import { ActionSheet, Button, Cell, Radio } from '@nutui/nutui-react-taro';
 
 import { Plus } from '@nutui/icons-react-taro';
 
 import './index.scss'
 
+
 const Demo = () => {
+  const [val, setVal] = useState('')
+  const [isVisible, setIsVisible] = useState(false);
+  const options = [
+    {
+      name: '权限设置',
+    },
+    {
+      name: '重命名',
+    },
+    {
+      name: '删除',
+    },
+  ]
 
-
+  const handleSelect = (item: any) => {
+    setVal(item.name)
+    setIsVisible(false)
+  }
 
   return <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', justifyItems: 'center'}}>
     <div className={'div_box'}>
@@ -25,24 +43,18 @@ const Demo = () => {
     </div>
 
     <div className={'div_box'}>
-      <div className={'div_box'}>请查看Radio css样式
-        right和 bottom 使用的样式变量都是 --nutui-radiogroup-radio-margin, 并没有使用文档上说的
-        --nutui-radiogroup-radio-margin-bottom
-
-      </div>
-      <Radio.Group defaultValue="1" direction={'horizontal'}>
-        <Radio shape="button" value="1">
-          选项1
-        </Radio>
-        <Radio shape="button" value="2">
-          选项2
-        </Radio>
-        <Radio shape="button" value="3">
-          选项3
-        </Radio>
-      </Radio.Group>
+      动作面板:
+      <Cell onClick={() => setIsVisible(!isVisible)}>
+        <span>基础用法</span>
+      </Cell>
     </div>
-
+    <ActionSheet
+      title="标题"
+      visible={isVisible}
+      options={options}
+      onSelect={handleSelect}
+      onCancel={() => setIsVisible(false)}
+    />
   </div>
 }
 export default Demo;
